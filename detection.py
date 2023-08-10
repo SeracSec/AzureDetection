37ec67fd2e1e66df4a83d9d6018ee8c0eb72fde5
SeracSec <matt.berluti@outlook.com>
2023-08-10 12:36:37 -0400

    Updated Azure AD presence to not only look for the domain.onmicrosoft.com. New Azure AD check gets all tenant domains and looks for a *.onmicrosoft.com domain. Output of that function is now Azure AD Domain name and tenant name.

--- detection.py
+++ detection.py
@@ -2,6 +2,8 @@ import requests
 import sys
 import colorama
 from colorama import Fore
+from xml.etree import ElementTree as ET
+
 
 def check_params(inputs):
     if len(inputs) == 2:
@@ -19,9 +21,49 @@ def get_tenant_id(domain):
     return requests.get(url).json()
     
 def check_azure_ad(domain):
-    cleaned = domain.split('.')[0]
-    url = f'https://login.microsoftonline.com/getuserrealm.srf?login=username@{cleaned}.onmicrosoft.com&json=1'
-    return requests.get(url).json()
+    # Create the XML body
+    body = f'''
+    <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"
+                xmlns:aut="http://schemas.microsoft.com/exchange/2010/Autodiscover">
+        <soap:Header>
+            <a:Action xmlns:a="http://www.w3.org/2005/08/addressing"
+                    soap:mustUnderstand="1">http://schemas.microsoft.com/exchange/2010/Autodiscover/Autodiscover/GetFederationInformation</a:Action>
+            <a:To xmlns:a="http://www.w3.org/2005/08/addressing"
+                soap:mustUnderstand="1">https://autodiscover-s.outlook.com/autodiscover/autodiscover.svc</a:To>
+            <a:ReplyTo xmlns:a="http://www.w3.org/2005/08/addressing">
+                <a:Address>http://www.w3.org/2005/08/addressing/anonymous</a:Address>
+            </a:ReplyTo>
+        </soap:Header>
+        <soap:Body>
+            <aut:GetFederationInformationRequestMessage xmlns:aut="http://schemas.microsoft.com/exchange/2010/Autodiscover">
+                <aut:Request>
+                    <aut:Domain>{domain}</aut:Domain>
+                </aut:Request>
+            </aut:GetFederationInformationRequestMessage>
+        </soap:Body>
+    </soap:Envelope>
+    '''
+
+    # Create the headers
+    headers = {
+        "Content-Type": "text/xml; charset=utf-8",
+        "SOAPAction": 'http://schemas.microsoft.com/exchange/2010/Autodiscover/Autodiscover/GetFederationInformation',
+        "User-Agent": "AutodiscoverClient"
+    }
+
+    # Make the request
+    response = requests.post("https://autodiscover-s.outlook.com/autodiscover/autodiscover.svc", data=body, headers=headers)
+
+    # Parse the response
+    xml_tree = ET.fromstring(response.text)
+    namespaces = {'a': 'http://schemas.microsoft.com/exchange/2010/Autodiscover'}
+
+    # Extract and sort the domains
+    domains = xml_tree.find('.//a:Domains', namespaces)
+    domain_list = [d.text for d in domains.findall('a:Domain', namespaces)]
+    domain_list.sort()
+
+    return domain_list
 
 def main():
     print('') #Spacing
@@ -54,11 +96,13 @@ def main():
 
     #Call function to check Azure AD presence and parse output
     azure_ad = check_azure_ad(domain)
-    if azure_ad['NameSpaceType'] == 'Managed':
+    for returned_domain in azure_ad:
+        if 'onmicrosoft.com' in returned_domain:
+            azure_ad_domain = returned_domain
+    if azure_ad_domain:
         print(Fore.GREEN + f'Azure AD presence detected for {domain}' + Fore.RESET)
-        print(f"Name Space Type: {azure_ad['NameSpaceType']}")
-        print(f"Domain Name: {azure_ad['DomainName']}")
-        print(f"Federation Brand Name: {azure_ad['FederationBrandName']}")
+        print(f"Azure AD Domain Name: {azure_ad_domain}")
+        print(f"Tenant Name: {azure_ad_domain.split('.')[0]}")
     else:
         print(Fore.RED + f'Azure AD presence not detected for {domain}' + Fore.RESET) 
     
